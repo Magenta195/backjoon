4b00cbef697e6ac1130a5ffb6e4761b1e62ec3ca
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2022-02-18 03:06:23 +0900

    Create 15652.py

---
+++ problem/sliver/15652.py
@@ -0,0 +1,23 @@
+###
+# 15652. N과 M(4)
+# problem : https://www.acmicpc.net/problem/15652
+# status : solved
+###
+
+n, m = map(int, input().split())
+s = []
+
+def dfs(d):
+  if len(s) == m:
+    print(' '.join(map(str, s)))
+    return
+  
+  for i in range(d, n+1):
+    if len(s) > 0 :
+      if s[-1] > i:
+        continue
+    s.append(i)
+    dfs(i)
+    s.pop()
+
+dfs(1)
