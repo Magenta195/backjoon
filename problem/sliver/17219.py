b5e47be779e92709175745ff079ad7f7d91e29d5
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2022-02-17 13:51:55 +0900

    Create 17219.py

---
+++ problem/sliver/17219.py
@@ -0,0 +1,16 @@
+
+###
+# 17219. 비밀번호 찾기
+# problem : https://www.acmicpc.net/problem/11727
+# status : solved
+###
+
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+dic = {}
+for _ in range(n):
+  site, pw = sys.stdin.readline().split()
+  dic[site] = pw
+for _ in range(m):
+  print(dic[sys.stdin.readline().strip()])
