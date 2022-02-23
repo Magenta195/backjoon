67aecaa46a11b4b576a191c0809803af2dfe13f6
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2022-02-23 15:59:46 +0900

    Update 1043.py

--- problem/gold/1043.py
+++ problem/gold/1043.py
@@ -5,29 +5,22 @@
 ###
 
 n, m = map(int, input().split())
-t_lst = list(map(int, input().split()))
-t_lst.pop(0)
+t, *t_lst = map(int, input().split())
+t_lst = set(t_lst)
 p_lst = [list(map(int, input().split())) for _ in range(m)]
 
 def search(t, f, s, cnt):
-  for i in t:
-    for j in f:
-      if i == j: return 0
+  if t.intersection(f): return 0
   if s == m :
     return cnt
   
   l = 0
   at = t.copy()
-  for i in p_lst[s][1:] :
-    at.append(i)
-  l = max(l, search(list(set(at)), f, s+1, cnt))
+  at.update(p_lst[s][1:])
+  l = max(l, search(at, f, s+1, cnt))
 
   af = f.copy()
-  for i in p_lst[s][1:] :
-    af.append(i)
-  l = max(l, search(t, list(set(af)), s+1, cnt+1))
+  af.update(p_lst[s][1:])
+  l = max(l, search(t, af, s+1, cnt+1))
   return l
-print(search(t_lst,[],0, 0))
-    
-  
-  
+print(search(t_lst,set(),0, 0))
