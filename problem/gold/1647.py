f7e5f79e4f839399d4cb310c45936d53f35e1d7b
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2022-03-02 14:08:07 +0900

    Update 1647.py

--- problem/gold/1647.py
+++ problem/gold/1647.py
@@ -1,43 +1,42 @@
 ###
 # 1647. 도시 분할 계획
 # problem : https://www.acmicpc.net/problem/1647
-# status : 
+# status : solved
 ###
 
-### trial 1(kruskal, 시간 초과)
+### trial 1(kruskal)
 import sys
-from heapq import heappush, heappop
 sys.setrecursionlimit(1000001)
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 v = list(range(n+1))
-q, d = [], []
-for a,b,c in [list(map(int, input().split())) for _ in range(m)]:
-  heappush(q, (c, a, b))
-
+d = 0
+q = sorted([list(map(int, input().split())) for _ in range(m)], key = lambda x : -x[2])
 def find(i):
   if v[i] == i : return i
   return find(v[i])
-  
+
 def union(x, y):
-  v[find(x)] = find(y)
+  ax = find(x)
+  ay = find(y)
+  if ax < ay : v[ay] = ax
+  else : v[ax] = ay
   
 cnt = 0
-while q and cnt < n-1 :
-  c,a,b = heappop(q)
+while q and cnt < n-2 :
+  a,b,c = q.pop()
   if find(a) == find(b) : continue
   cnt += 1
   union(a, b)
-  heappush(d, -c)
-heappop(d)  
-print(-sum(d))
+  d += c
 
-### trial 2
+print(d)
+
+### trial 2(prim, 시간 초과)
 
 import sys
 from heapq import heappush, heappop
-sys.setrecursionlimit(1000001)
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -47,16 +46,16 @@ for a,b,c in [list(map(int, input().split())) for _ in range(m)]:
   g[a].append((b,c))
   g[b].append((a,c))
 
-q, d = [(0,1,1)], []  
+q, d = [(0,1,1)], 0
   
 cnt = 0
 while q and cnt < n-1 :
   c, a, b = heappop(q)
-  cnt += 1
-  v[b] = True
-  heappush(d, -c)
-  for k, k_c in g[b]:
-    if not v[k] : heappush(q, (b, k, k_c))
-heappop(d)
+  if not v[b] :
+    cnt += 1
+    v[b] = True
+    d += c
+    for k, k_c in g[b]:
+      heappush(q, (k_c, b, k))
     
-print(-sum(d))
+print(d)
