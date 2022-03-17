5a0a8a149e0c137132851515c9b83bc639b31cdb
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2022-03-17 14:45:34 +0900

    Update 1799.py

--- problem/gold/1799.py
+++ problem/gold/1799.py
@@ -45,43 +45,30 @@ while q:
 print(s)
 
 
-### trial 2(시도중)
-dx = [1, 1, -1, -1]
-dy = [1, -1, 1, -1]
-
+### trial 2(이분 탐색)
 n = int(input())
 lst = [list(map(int, input().split())) for _ in range(n)]
- 
-q = set()
-one_lst = set()
 
+dic = { keys : [] for keys in range(2*n-1)}
+match = [-1]*(2*n-1)
 for i in range(n):
   for j in range(n):
     if lst[i][j] == 1 :
-      one_lst.add((j,i))
-      q.add(((j,i),))
+      dic[j+i].append(j-i+n-1)
 
-s = 0
-print(one_lst)
-print(q)
-while q:
-  tup = q.pop()
-  s = max(s, len(tup))
-  abd_lst = set(tup)
-  avail_lst = one_lst - abd_lst
+def dfs(a):
+  if visited[a]:
+    return False
+  visited[a] = True
 
-  for ax, ay in avail_lst :
-    flg = False
-    for k in range(4) :
-      tx, ty = dx[k], dy[k]
-      tax, tay = ax + tx, ay + ty
-      while -1<tax<n and -1<tay<n:
-        if (tax,tay) in abd_lst:
-          flg = True
-          break
-        tax, tay = tax + tx, tay + ty
-      if flg : break
-    if flg : continue
-    q.add(tup+((ax,ay),))  
+  for b in dic[a] :
+    if match[b] == -1 or dfs(match[b]):
+      match[b] = a
+      return True
+  return False
 
-print(s)
+for i in range(2*n-1):
+  visited = [False]*(2*n-1)
+  dfs(i)
+
+print(len([i for i in match if i > -1]))
