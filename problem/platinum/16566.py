680c4c90a69c0994a22582befa749aebd60fa64a
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2022-03-18 10:29:03 +0900

    Create 16566.py

---
+++ problem/platinum/16566.py
@@ -0,0 +1,34 @@
+###
+# 16566. 카드 게임
+# problem : https://www.acmicpc.net/problem/16566
+# status : 
+###
+import sys
+sys.setrecursionlimit(10000000)
+
+N,M,K = map(int, input().split())
+card = sorted(list(map(int, input().split())))
+parent = list(range(M))
+K_card = list(map(int, input().split()))
+
+def find(a):
+  if parent[a] == a:
+    return a
+  parent[a] = find(parent[a])
+  return parent[a]
+
+def union(a, a1):
+  parent[a] = a1
+
+for k in K_card :
+  start, end = 0, M-1
+  while start < end :
+    mid = (start + end) // 2
+    if card[mid] <= k :
+      start = mid + 1
+    else :
+      end = mid
+  v = find(end)
+  if v < M-1 : v1 = find(v+1)
+  print(card[v])
+  if v < M-1 : union(v, v1)
