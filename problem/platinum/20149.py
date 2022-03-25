c3c7210c8948a336943c60ecea162d3a03c3a3d2
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2022-03-25 13:07:57 +0900

    Update 20149.py
    problem solved

--- problem/platinum/20149.py
+++ problem/platinum/20149.py
@@ -1,13 +1,13 @@
 ###
 # 20149. 선분 교차 3
 # problem : https://www.acmicpc.net/problem/20149
-# status : NOT solved
+# status : solved
 ###
 
 def ccw(a, b, c):
   return (a[0]*b[1]+b[0]*c[1]+c[0]*a[1]) - (a[1]*b[0]+b[1]*c[0]+c[1]*a[0])
 
-def check(a, b, c, d)
+def check(a, b, c, d):
   c1 = ccw(a,b,c)
   c2 = ccw(a,b,d)
   c3 = ccw(c,d,a)
@@ -20,20 +20,28 @@ def check(a, b, c, d)
       c,d=d,c
     if a <= d and c <= b:
       if a == c or b == d :
-        return 1
+        m1 = (b[1]-a[1])/(b[0]-a[0]) if b[0] != a[0] else float('inf')
+        m2 = (d[1]-c[1])/(d[0]-c[0]) if c[0] != d[0] else float('inf')
+        if m1 == m2 : return 1, 
+        else : return (1, *a) if a == c else (1, *b)
       elif a == d or b == c :
-        return (1, a) if a == d else (1, c)
-      else : return 1
+        return (1, *a) if a == d else (1, *b)
+      else : return 1, 
     else :
-      return 0
+      return 0, 
   elif c1*c2 <= 0 and c3*c4 <= 0:
-    m1 = 
-    return 1, ((a[0]*
-  else : return 0
+    if a[0] == b[0] :
+      m = (d[1]-c[1])/(d[0]-c[0])
+      return 1, a[0], m*(a[0]-c[0])+c[1]
+    if c[0] == d[0] :
+      m = (b[1]-a[1])/(b[0]-a[0])
+      return 1, c[0], m*(c[0]-a[0])+a[1]
+    m1, m2 = (b[1]-a[1])/(b[0]-a[0]), (d[1]-c[1])/(d[0]-c[0])
+    x = ((m1*a[0]-m2*c[0])-(a[1]-c[1]))/(m1-m2)
+    return 1, x, m1*x-m1*a[0]+a[1]
+  else : return 0, 
  
 lst = [list(map(int, input().split())) for _ in range(2)]
 a, *chk = check(lst[0][:2], lst[0][2:], lst[1][:2], lst[1][2:])
 print(a)
 if chk : print(*chk)
-  
-                                              
