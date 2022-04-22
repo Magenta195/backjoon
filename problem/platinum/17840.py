79bd8d79e4296ab1b1d7b313da6a8b6eb530238b
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2022-04-22 15:40:16 +0900

    Create 17840.py

---
+++ problem/platinum/17840.py
@@ -0,0 +1,25 @@
+###
+# 17840. 피보나치 음악
+# problem : https://www.acmicpc.net/problem/17840
+# status : solved
+###
+
+import sys
+input = sys.stdin.readline
+
+Q, M = map(int, input().split())
+dp = [[-1]*M for _ in range(M)]
+dp[0][1] = 0
+lst = [1]
+pre, now, cnt = 1, 1, 1
+
+while dp[pre][now] == -1 :
+  dp[pre][now] = cnt
+  tmp = list(map(int, str(now)))
+  lst += tmp
+  cnt += len(tmp)
+  pre, now = now, (pre+now) % M
+base, mod = dp[pre][now], cnt - dp[pre][now]
+for _ in range(Q):
+  num = int(input())
+  print(lst[num-1] if num < cnt else lst[(num - base) % mod + base - 1])
