228056740561ad691ac84294a3bab0d2aecb8722
Magenta195 <58595201+Magenta195@users.noreply.github.com>
2022-05-14 17:44:46 +0900

    Update 14891.py

--- problem/gold/14891.py
+++ problem/gold/14891.py
@@ -5,27 +5,18 @@
 ###
 
 from collections import deque
-
-lst = [deque(list(input())) for _ in range(4)]
-lst 
-K = int(input())
-ans = 0
-for _ in range(K):
-  n, d = map(int, input().split())
-  nd, nv = d, lst[n-1][2]
+lst=[deque(list(input())) for _ in range(4)]
+for _ in range(int(input())):
+  n,d=map(int,input().split())
+  nd,nv=d,lst[n-1][2]
   for i in range(n-1,-1,-1) :
     v = lst[i][-2]
     lst[i].rotate(nd)
-    if i > 0 and lst[i-1][2] != v :
-      nd = -nd
-    else :
-      break
-  for i in range(n, 4):
-    if lst[i][-2] != nv :
-      nv = lst[i][2]
-      d = -d
+    if i>0 and lst[i-1][2] != v : nd=-nd
+    else:break
+  for i in range(n,4):
+    if lst[i][-2]!=nv:
+      nv,d=lst[i][2],-d
       lst[i].rotate(d)
-    else :
-      break  
-
-print(sum([2**i for i, x in enumerate(lst) if x[0] == '1']))
+    else:break
+print(sum([2**i for i,x in enumerate(lst) if x[0]=='1']))
